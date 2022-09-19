86cbaa898540fc3b7c56872857817a108a895108
HKjain <harshkawadia0609@gmail.com>
2022-09-19 17:26:59 +0530

    Question added in 4th assignment

--- Assignments/Assignment 4/a4_q1.js
+++ Assignments/Assignment 4/a4_q1.js
@@ -1,8 +1,13 @@
+/*
+Create a function that takes a number (step) as an argument and returns the
+number of matchsticks in that step.
+*/
 var userInput = 5;
 // the below line runs in console of browser but not in terminal of VSCode
 // var userInput = prompt("Enter a number: ");
 
 function numberOfMatchsticks(n) {
+  if (n === 0) return 0;
   if (n === 1) return 6;
   return numberOfMatchsticks(n - 1) + 5;
 }
