19381e7062f8deafdef7331852b767c63a9415ce
HKjain <harshkawadia0609@gmail.com>
2022-11-23 18:59:07 +0530

    Assignment 9 uploaded

---
+++ Assignments/Assignment 9/assignment.js
@@ -0,0 +1,19 @@
+// Q-1
+// a): yes InnerFunction is a closure
+// b): Output- it will pop an alert box with 100 as value
+
+// Q-2
+// Scope specifies the block or the part of program where we can access a variable where as
+// Closure is a function enclosed within a function where it reference to the variable of its outer scope
+
+// Q-3
+// Lexical scope is used to resolve the variable names when a closure is created.
+// Lexical determines the scope of a function by looking at where we created the function.
+
+// Q-4
+/*
+Output:
+3
+3
+3
+*/
