9aeaa9a391d77c49874f59c9f7a56ba68b78275d
HKjain <harshkawadia0609@gmail.com>
2022-11-23 20:35:41 +0530

    Assignment 10 uploaded

---
+++ Assignments/Assignment 10/assignment.js
@@ -0,0 +1,33 @@
+// Q-1
+/*
+Higher order function and callback function are different but they are related to each other.
+A higher order function is a function which takes another function(s) as argument and/or returns a function
+whereas A callback function is a function which is passed as an argument
+*/
+
+// Q-2
+/*
+Yes, filter is a higher order function.
+Since filter takes a function as an argument to specify the condition for filtering.
+*/
+
+// Q-3
+// function execute() {
+//   console.log("hello world");
+// }
+// function helloWorld(callback) {
+//   callback();
+// }
+// helloWorld(execute);
+
+// Q-4
+/*
+a)
+Hello John
+Hello Tina
+Hello Kale
+Hello Max
+
+b)
+useFunction is the higher order function here
+*/
